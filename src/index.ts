b9a56dcf67dfb0ba7c4e8f29bd6db94456de0719
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-08 09:00:59 -0500

    :bookmark: v0.1.1

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
-import { readFileSync } from "fs";
-import { readFile } from "fs/promises";
+import { readFileSync } from "node:fs";
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
 
-export function readFromHereSync(filename: string) {
-  return readFileSync(`${__dirname}/${filename}`, "utf8");
+export function readFromSync(
+  from: string,
+  filename: string,
+  format: BufferEncoding = "utf8"
+) {
+  return readFileSync(join(from, filename), format);
 }
 
-export function readFromHere(filename: string) {
-  return readFile(`${__dirname}/${filename}`, "utf8");
+export function readFromSyncGenerator(from: string) {
+  return (filename: string, format: BufferEncoding = "utf8") => {
+    return readFileSync(join(from, filename), format);
+  };
+}
+
+export function readFrom(
+  from: string,
+  filename: string,
+  format: BufferEncoding = "utf8"
+) {
+  return readFile(join(from, filename), format);
+}
+
+export function readFromGenerator(from: string) {
+  return (filename: string, format: BufferEncoding = "utf8") => {
+    return readFile(join(from, filename), format);
+  };
 }
